aa37e7ed83bdbbce8f25a864e806bcdd3d361446
= <=>
2022-09-08 22:05:59 +0700

    Jump V1.0

--- GameScene.js
+++ GameScene.js
@@ -1,3 +1,4 @@
+
 class GameScene extends Phaser.Scene {
     constructor() {
         super({ key: "GameScene" });
@@ -16,95 +17,100 @@ class GameScene extends Phaser.Scene {
 
         this.player.setBounce(0.9,0)
         this.player.setCollideWorldBounds(true)
-        this.player.body.setFrictionX(0)
+        // this.player.body.setFrictionX(0)
 
         // PlatForm
 
         this.platform = this.physics.add.staticGroup().setOrigin(0.5,0.5)
         this.platform.create(640, 700,"Platform")
+        this.platform.create(0, 500,"Platform")
+        this.platArr = []
 
-        // craete ground check zone
-        this.groundCheck = this.add.zone(0, 0).setSize(this.player.width, 5)
-        this.physics.world.enable(this.groundCheck)
-        this.groundCheck.body.setAllowGravity(false)
+        for (let i in this.platform.getChildren()) {
+            this.platArr[i] = this.platform.getChildren()[i].body.top
+        }
 
-        this.physics.add.overlap(this.groundCheck, this.platform, () => {
-            this.jumpToken = this.time.now
-        })
+        this.jumpAble = true
 
         // Collinder
 
         this.physics.add.collider(this.player,this.platform)
 
-        // Time Event
-        this.jumpTime = new Phaser.Time.TimerEvent({ delay: 1200 })
-        this.jumpAble = true
+
 
         // Keyboard Input
         this.left = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A)
         this.right = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D)
         this.space = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)
         this.enter = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER)
+
+
     }
 
     update() {
         this.movement()
-        this.Ground()
-        console.log(this.jumpAble)
-
-        this.progress = this.jumpTime.getProgress()
-        this.jumpForce = Math.ceil(this.progress * 5 ) //Progression is value between 0 and 1. We can extend gap by multiply it. Range will be rise but time will not change at all
-        this.jumpdebug.setText(["Time : " + this.progress.toString().substring(0, 4),"JumpForce : ",this.jumpForce])
     }
 
     movement() {
         // X axis Movement
-        
-        if (this.Grounded())
-        {
-            if (this.left.isDown) { 
-                this.player.setFlipX(true)
-                this.player.setVelocityX(-playerSpeed)
-            } else if (this.right.isDown) {
-                this.player.setFlipX(false)
-                this.player.setVelocityX(playerSpeed)
+        if (this.Grounded()){
+            this.dur = this.space.duration
+            if (!this.space.isDown) {
+               if (this.left.isDown) { 
+                    this.player.setFlipX(true)
+                    this.player.setVelocityX(-playerSpeed)
+                } else if (this.right.isDown) {
+                    this.player.setFlipX(false)
+                    this.player.setVelocityX(playerSpeed)
+                } else {
+                    this.player.setVelocityX(0)
+                } 
             } else {
                 this.player.setVelocityX(0)
             }
-
+            
             // Y axis Movement
-            
-            if (Phaser.Input.Keyboard.JustDown(this.space)) {
-                this.time.addEvent(this.jumpTime)
-            } 
-            else if (this.space.isDown) {
-                console.log('Isdown')
-                this.player.setVelocityX(0)
-                if (this.progress == 1 && this.jumpAble) 
-                {
-                    this.player.setVelocityY(-150 * this.jumpForce)
+            if (!this.space.isDown) this.jumpAble = true
+            if (this.space.getDuration() >= 1200 && this.jumpAble) {
+                this.player.setVelocity(this.jumpDir(),-playerSpeed * 5)
+                    this.space.duration = 0
                     this.jumpAble = false
-                }
-            } else if (Phaser.Input.Keyboard.JustUp(this.space) && this.jumpAble){
-                this.player.setVelocityY(-150 * this.jumpForce)
-                this.jumpAble = false
-            }
-
-            if (!this.space.isDown) {
-                this.jumpAble = true
             }
             
+            switch (true) {
+                case (this.dur / 240) == 0 :
+                    break
+                case (this.dur / 240) < 1 :
+                    this.player.setVelocity(this.jumpDir(),-playerSpeed)
+                    this.space.duration = 0
+                    this.jumpAble = false
+                    break
+                case (this.dur / 240) < 2 :
+                    this.player.setVelocity(this.jumpDir(),-playerSpeed * 2)
+                    this.space.duration = 0
+                    this.jumpAble = false
+                    break
+                case (this.dur / 240) < 3 :
+                    this.player.setVelocity(this.jumpDir(),-playerSpeed * 3)
+                    this.space.duration = 0
+                    this.jumpAble = false
+                    break
+                case (this.dur / 240) < 4 :
+                    this.player.setVelocity(this.jumpDir(),-playerSpeed * 4)
+                    this.space.duration = 0
+                    this.jumpAble = false
+                    break
+            }
         } 
     }
 
-    // Make ground checker follow player
-    Ground() {
-        this.groundCheck.x = this.player.x
-        this.groundCheck.y = this.player.y + this.player.height/2
-    }
 
     Grounded() {
-        return this.jumpToken == this.time.now
+        return this.platArr.includes(this.player.body.bottom)
+    }
+
+    jumpDir() {
+        return !this.left.isDown ? this.right.isDown ? playerSpeed : 0 : -playerSpeed 
     }
 
 }
